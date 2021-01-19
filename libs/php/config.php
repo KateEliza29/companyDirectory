5a68c3171dcb47b6e0410c1040c410325a3bad15
Kate Eliza <57634902+KateEliza29@users.noreply.github.com>
2021-01-19 14:10:43 +0000

    Delete config.php

--- libs/php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$cd_host = "localhost";
-	$cd_port = 3308;
-	$cd_socket = "";
-	$cd_user = "root"; // user name
-	$cd_password = ""; // password sElling@348
-	$cd_dbname = "companydirectory"; // database name
-?>
